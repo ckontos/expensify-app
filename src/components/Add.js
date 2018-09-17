e3d03da2b1399f41117b8aa5c26e660fff735cbf
Dimitri Kontos <ckontos@rocketmail.com>
2018-09-17 21:34:58 +0000

    production webpack build added

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses'
+import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
-  onSubmit=(expense) => {
-        this.props.addExpense(expense);
+  onSubmit = (expense) => {
+        this.props.onSubmit(expense);
         this.props.history.push('/');
   };
   render() {
@@ -20,8 +20,8 @@ export class AddExpensePage extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  addExpense: (expense) => dispatch(addExpense(expense))
-};
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense))
+});
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
