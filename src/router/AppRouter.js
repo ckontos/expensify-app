d0b675d4514cf434765ce72456760f8c5dbbed94
Dimitri Kontos <ckontos@rocketmail.com>
2018-10-22 20:00:08 +0000

    First authentication logic added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
-import Header from '../components/Header';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/Dashboard';
 import AddExpensePage from '../components/Add';
 import EditExpensePage from '../components/Edit';
 import HelpPage from '../components/Help';
 import NotFound from '../components/Error';
+import LoginPage from '../components/Login';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
 
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-            <Header />
             <Switch>
-                <Route path='/' component={ ExpenseDashboardPage } exact = {true} />
-                <Route path='/create' component={ AddExpensePage } />
-                <Route path='/edit/:id' component={ EditExpensePage } />
+                <Route path='/' component={ LoginPage } exact = {true} />
+                <PrivateRoute path='/dashboard' component={ ExpenseDashboardPage } />
+                <PrivateRoute path='/create' component={ AddExpensePage } />
+                <PrivateRoute path='/edit/:id' component={ EditExpensePage } />
                 <Route path='/help' component={ HelpPage } />
                 <Route component={ NotFound } />
             </Switch>
         </div>
-    </BrowserRouter>
+    </Router>
 
 );
 
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {addExpense, editExpense, startEditExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -24,6 +24,22 @@ test('Should setup remove expense action object', () => {
     });
 });
 
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[2].id;
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toBeFalsy();
+        done();
+    });
+});
+
 test('Should set up edit expense action object', () => {
     const action = editExpense('123abc', { note: 'new note' });
     expect(action).toEqual({
@@ -34,6 +50,26 @@ test('Should set up edit expense action object', () => {
         }
     });
 });
+
+test('should edit expense in firebase', (done) => {
+    const store = createMockStore();
+    const id = expenses[0].id;
+    const updates = { amount: 21045 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+    });
+});
+
+
 
 test('Should set up the add expense action object with given values', () => {
     const action = addExpense(expenses[2]);
@@ -68,7 +104,7 @@ test('should add expense to database and store', (done) => {
     });
 });
 
-test('should add expense with default values to database and store', () => {
+test('should add expense with default values to database and store', (done) => {
     const store = createMockStore({});
     const expenseDefaults = {
         description: '',
@@ -112,3 +148,4 @@ test('should fetch data from firebase', (done) => {
         done();
     });
 });
+
